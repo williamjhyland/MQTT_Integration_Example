cba3d2968dfdd32f6f79f1db3e876bc2cb2a448c
williamjhyland <williamjhyland@outlook.com>
2023-11-02 16:28:24 -0400

    Broker

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from viam.components.sensor import Sensor
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.logging import getLogger
 
-from src.MQTT_Client import MQTT_Integration 
+from src.MQTT_Client import MQTT_Client 
+from src.MQTT_Broker import MQTT_Broker 
 
 logger = getLogger(__name__)
 
@@ -17,9 +18,11 @@ async def main():
     """This function creates and starts a new module, after adding all desired resources.
     Resources must be pre-registered. For an example, see the `__init__.py` file.
     """
-    Registry.register_resource_creator(Sensor.SUBTYPE, MQTT_Integration.MODEL, ResourceCreatorRegistration(MQTT_Integration.new))
+    Registry.register_resource_creator(Sensor.SUBTYPE, MQTT_Client.MODEL, ResourceCreatorRegistration(MQTT_Client.new))
+    Registry.register_resource_creator(Sensor.SUBTYPE, MQTT_Broker.MODEL, ResourceCreatorRegistration(MQTT_Broker.new))
     module = Module.from_args()
-    module.add_model_from_registry(Sensor.SUBTYPE, MQTT_Integration.MODEL)
+    module.add_model_from_registry(Sensor.SUBTYPE, MQTT_Client.MODEL)
+    module.add_model_from_registry(Sensor.SUBTYPE, MQTT_Broker.MODEL)
     await module.start()
 
 
